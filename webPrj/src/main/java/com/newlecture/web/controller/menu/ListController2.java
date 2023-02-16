1169ac8c28dcaae0527f552e47e82e87605fd8d8
joobro <joobro2015@gmail.com>
2023-02-16 18:13:56 +0900

    Newlec

---
+++ webPrj/src/main/java/com/newlecture/web/controller/menu/ListController2.java
@@ -0,0 +1,138 @@
+package com.newlecture.web.controller.menu;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.newlecture.web.entity.GList;
+import com.newlecture.web.entity.Menu;
+import com.newlecture.web.service.MenuService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/menu/list2")
+public class ListController2 extends HttpServlet{
+	
+	private MenuService service;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<Menu> menus = new ArrayList<Menu>();
+		
+//		NList menus = new NList();
+		
+		resp.setCharacterEncoding("UTF-8");
+		//브라우저에서 html로 읽고 UTF-8로 읽어라 하고 헤더에 넣어줌.
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		out.print("hello");
+		
+//		Menu[] list = service.getList();
+//		int count = service.count();
+
+		String query = "";
+		String sql = String.format("select * from member where nicname like '%s'", "%"+query+"%") ;
+//		List<Menu> list = new ArrayList<>();
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@oracle.newlecture.com:1521/xepdb1";
+			Connection con = DriverManager.getConnection(url, "NEWLEC", "rland");
+			
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			
+//			Menu[] menus = new Menu[100];
+			
+			
+			
+			
+			// 필터링, 집계, 정렬
+			while(rs.next())	// 서버의 커서를 한칸 내리고 그 위치의 레코드를 옮겨 오는 것. -> 레코드 하나가 저장되는 공간은?
+			{
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				String nicName = rs.getString("nicname");
+				Date regDate = rs.getDate("reg_date");
+				String images = "pic1.png,pic2.png,pic3.png,pic4.png,pic5.png";
+				
+				Menu menu = new Menu(id, name, 1000, "", regDate);
+				menu.setImages(images);
+				
+				menus.add(menu);
+				
+//				menus.add(3);
+			}
+			
+			con.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		req.setAttribute("menus", menus);
+//		resp.sendRedirect("listView");
+		//response 출력도구
+		//sendRedirect 다른곳에 정보전달
+		// list2를 요청받았지만 listview로 찾아가라고 재응답
+//		 forward 방법 서버에서 서버로 바로 요청
+		req.getRequestDispatcher("/WEB-INF/view/menu/list.jsp")
+		.forward(req, resp);
+		
+//		forward를 통해 두개의 객체를 공유
+//		list2를 요청했지만 잠시만 기다려라고하고
+//		listview를 물어봐서 들고옴
+		
+//		셰션, 쿠키 쿼리스트링, 리퀘스트 히든필드
+		
+//		페이지저장소 리퀘스트저장소 세션 저장소 어플리케이션 저장소 서버상 데이터 저장을 위한 프로
+//		============================================
+		
+//		out.write("<!DOCTYPE html>");
+//		out.write("<html>");
+//		out.write("<head>");
+//		out.write("<meta charset=\"UTF-8\">");
+//		out.write("<title>Insert title here</title>");
+//		out.write("</head>");
+//		out.write("<body>");
+//		out.write("<h1>메뉴 목록</h1>");
+//		out.write("<table>");
+//		out.write("<tr>");
+//		out.write("<td>번호</td>");
+//		out.write("<td>이름</td>");
+//		out.write("<td>가격</td>");		
+//		out.write("</tr>");
+//		
+//		for(int i=0; i<menus.size(); i++) {
+//			// 형식 변환
+//			Menu m = (Menu) menus.get(i);
+//			
+//		out.write("<tr>");	
+//		out.write("	<td>"+m.getId()+"</td>");	
+//		out.write("	<td>"+m.getName()+"</td>");	
+//		out.write("	<td>5000</td>");				
+//		out.write("</tr>");
+//		}
+//		
+//		out.write("</table>");	
+//		out.write("</body>");	
+//		out.write("</html>");	
+		
+	}
+}
---
+++ webPrj/src/main/java/com/newlecture/web/entity/Menu.java
@@ -0,0 +1,104 @@
+package com.newlecture.web.entity;
+
+import java.util.Date;
+
+public class Menu {
+	private int id;
+	private String name;
+	private int price;
+	private String img;
+	private Date regDate;
+	private String images;
+	
+	static {
+		
+	}
+	
+	public Menu() {
+		Menu menu = new Menu();
+//		DriverManager.registerDriver(this);
+	}
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+
+
+	public Menu(int id, String name, int price, String img, Date regDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.img = img;
+		this.regDate = regDate;
+	}
+
+
+
+	public Date getRegDate() {
+		return regDate;
+	}
+
+
+
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+
+
+
+	public String getImages() {
+		return images;
+	}
+
+
+
+	public void setImages(String images) {
+		this.images = images;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Menu [id=" + id + ", name=" + name + ", price=" + price + ", img=" + img + ", regDate=" + regDate
+				+ ", images=" + images + "]";
+	}
+
+
+
+	
+	
+	
+	
+	
+	// 생성자(기본, 오버로드) 
+	// 켓더세터    
+	// toString(); -> 개체값을 문자로 뽑아내는
+	// 아묻따 저 세 개 만들기
+	
+}
